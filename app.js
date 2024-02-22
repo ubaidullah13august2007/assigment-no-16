5a8b1e8cb31ad60364711e1997e8c32d7ad80fec
ubaidullah13august <abdulkhan1311@gmail.com>
2024-02-22 10:01:24 +0500

    asigment 16

---
+++ app.js
@@ -0,0 +1,69 @@
+// alert(10)
+
+// document.write(100);
+
+// console.log(30)
+
+// console.log('faraz')
+
+// alert(typeof 12)
+
+// document.write(typeof 'faraz')
+
+// console.log(typeof true)
+
+// var a = 290
+
+// alert(a)
+
+// var firstname = "muhammad"
+// var lastname = "faraz"
+
+// console.log(firstname + ' ' + lastname)
+// var number = 50
+// console.log(number)
+  
+// chapter no 1
+// Q no 1 and 2
+alert("Error!Please Enter a Valid Password")
+// Q no 3
+alert("Welcom To Js Land...\nHappy Coding!")
+// Q no 4
+alert("Welcome To Js Land" )
+alert('Happy Coding!\nPrevent this page from Creating Additional Dialogs.')
+// Q no 5
+alert("Hello...I Can Run Js through by web browser's console" )
+// Q no 6
+alert()
+// Chapter no 2
+// Q no 1
+var username="ubaid"
+// Q no 2
+var myname="ubaidullah"
+// Q no 3
+var message="hello world"
+// alert(message)
+// Q no 4
+var name="jhon doe" 
+alert(name)
+// var age=15
+alert(age,)
+alert("certified mobile application Development")
+// Q no 5
+var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+// Q no 6
+var email=" example@example.com"
+alert("my email address is"+email)
+// Q no 7
+var book=" A Smarter\nway to learn java script"
+alert("i am trying to learn from a book"+book)
+// Q no 8
+// document.write("Yah! I can write HTML content through JavaScript")
+// chapter no 3
+// Q no 1
+var age=15
+alert("I am "+age+" years old")
+// Q no 2
+var n=14
+alert("you have visited this site "+n+" times" )
